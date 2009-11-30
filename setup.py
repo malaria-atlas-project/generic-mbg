819f3e3bc4bb882cc593ee8b417291ed4de7b7e1
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-11-30 12:18:29 +0000

    setup.py smarter about figuring out path to Python executable

--- setup.py
+++ setup.py
@@ -19,6 +19,10 @@ config = Configuration('generic_mbg',parent_package=None,top_path=None)
 
 config.add_extension(name='histogram_utils',sources=['generic_mbg/histogram_utils.f'])
 
+def get_syscall_output(str):
+    process = subprocess.Popen(str, stdout=subprocess.PIPE, shell=True)
+    os.waitpid(process.pid, 0)
+    return process.stdout.read().strip()
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
@@ -32,13 +36,12 @@ if __name__ == '__main__':
             **(config.todict()))
     for ex_fname in ['mbg-map','mbg-validate','mbg-scalar-priors','mbg-realize-prior','mbg-covariate-traces','mbg-decluster','get_declustered_sample.R','mbg-infer','mbg-describe-tracefile']:
 
-        process = subprocess.Popen('git show --pretty=format:"%H" --quiet', stdout=subprocess.PIPE, shell=True)
-        os.waitpid(process.pid, 0)
-        commit = process.stdout.read().strip()
+        commit = get_syscall_output('git show --pretty=format:"%H" --quiet')
+        pythonpath = get_syscall_output('which python')
 
         path_fname = os.path.join(prefix,ex_fname)
         os.system('rm %s'%path_fname)
-        file(path_fname,'w').write('#!/usr/bin/python\ngeneric_commit = "%s"\n'%commit)
+        file(path_fname,'w').write('#!%s\ngeneric_commit = "%s"\n'%(pythonpath, commit))
         os.system('cat %s >> %s'%(ex_fname,path_fname))
         os.system('chmod ugo+x %s'%path_fname)
 
