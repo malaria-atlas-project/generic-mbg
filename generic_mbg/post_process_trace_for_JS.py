34acc1f854637cb8a70e2d6b3962942f55806d29
root <peter.gething@zoo.ox.ac.uk>
2011-01-11 15:49:57 +0000

    minor

--- generic_mbg/post_process_trace_for_JS.py
+++ generic_mbg/post_process_trace_for_JS.py
@@ -4,7 +4,7 @@ import numpy as np
 ##############################################################################################
 # utiliy function
 def check_if_chain (child_name):
-    if (child_name.find('chain') != -1):
+    if(child_name.find('chain') != -1):
         return (True)
     return (False)
 check_if_chain_v=np.vectorize(check_if_chain)
@@ -22,11 +22,11 @@ def collapse_trace_to_chain0(hf_path):
     # if we don;t have more than one chain, simply return
     if(nchains==0):
         raise ValueError ('No chains found')
-        
+
     if(nchains==1):
         print('No additional chains found, returning')
         return(0)
-        
+
     # if we have multiple chains, define list of their names above chain0
     extra_chain_names=child_list[ischain]
     extra_chain_names=extra_chain_names[extra_chain_names!='chain0']
@@ -43,60 +43,82 @@ def collapse_trace_to_chain0(hf_path):
         if(PyMCsamples.colnames!=hf.root.chain0.PyMCsamples.colnames):
             raise ValueError ('colnames of '+extra_chain_name+' do not match chain0')
 
-        # loop through 'columns' of PyMCsamlpes table, appending to those of chain0
-        for colname in PyMCsamples.colnames:
-       
-            # get this column of chain0 (check no hanging dimensions)
-            col_0=hf.root.chain0.PyMCsamples.col(colname)
-            col_0=col_0.squeeze()
+#        # loop through 'columns' of PyMCsamlpes table, appending to those of chain0
+#        for colname in PyMCsamples.colnames:
+#
+#            # get this column of chain0 (check no hanging dimensions)
+#            col_0=hf.root.chain0.PyMCsamples.col(colname)
+#            col_0=col_0.squeeze()
+#
+#            # get this column of this additional chain (check no hanging dimensions)
+#            col_new=PyMCsamples.col(colname)
+#            col_new=col_new.squeeze()
+#
+#            # check columns have same dimensionality
+#            if( len(col_0.shape) != len(col_new.shape) ):
+#                raise ValueError ('column '+colname+' dimension mis-match: chain0 = '+str(col_0.shape)+'; '+extra_chain_name+' = '+str(col_new.shape))
+#
+#            # concatenate new col to chain0
+#            if(len(col_0.shape)==1):
+#                col_conc=np.hstack((col_0,col_new))
+#
+#            if(len(col_new.shape)==2):
+#                col_conc=np.vstack((col_0,col_new))        
+#
+#            if((len(col_new.shape)!=1) & (len(col_new.shape)!=2)):
+#                raise ValueError('Something fishy going on: column '+colname+' is not 1-d or 2-d')
+#
+#            # overwrite this col of chain0 with this concatenated version
+#            col_0=col_conc
 
-            # get this column of this additional chain (check no hanging dimensions)
-            col_new=PyMCsamples.col(colname)
-            col_new=col_new.squeeze()
-            
-            # check columns have same dimensionality
-            if( len(col_0.shape) != len(col_new.shape) ):
-                raise ValueError ('column '+colname+' dimension mis-match: chain0 = '+str(col_0.shape)+'; '+extra_chain_name+' = '+str(col_new.shape))
+        # initialise row iterator
+        trow=hf.root.chain0.PyMCsamples.row
 
-            # concatenate new col to chain0
-            if(len(col_0.shape)==1):
-                col_conc=np.hstack((col_0,col_new))
+        # loop through number of rows (realisations) in this additional Chain PyMCsamples table
+        
+        jj=0
+        for ii in xrange(0,10):        #PyMCsamples.nrows
+            jj=jj+1
+            if (jj==10):
+                print('on row '+str(ii)+' of '+str(PyMCsamples.nrows))
+                jj=0
 
-            if(len(col_new.shape)==2):
-                col_conc=np.vstack((col_0,col_new))        
+            # loop through columns and copy across this row of each
+            for colname in PyMCsamples.colnames:
+                trow[colname]=hf.root.chain1.PyMCsamples.col(colname)[ii]
 
-            if((len(col_new.shape)!=1) & (len(col_new.shape)!=2)):
-                raise ValueError('Something fishy going on: column '+colname+' is not 1-d or 2-d')
+            # append this row to target table
+            trow.append()
 
-            # overwrite this col of chain0 with this concatenated version
-            col_0=col_conc
+        # flush table to implement changes 
+        hf.root.chain0.PyMCsamples.flush()
 
 
-        # next deal with group0 table
-        group0=chain.group0
-
-        # check nodes matches those of Chain0.group0
-        if(group0._v_children.keys()!=hf.root.chain0.group0._v_children.keys()):
-            raise ValueError ('children of '+extra_chain_name+' do not match chain0')
-
-        # loop through nodes of this chain's group0 group, appending to those of chain0
-        for nodename in group0._v_children.keys():
-
-            # get this column of chain0
-            node_0=hf.root.chain0.group0._f_getChild(nodename) 
-
-            # get this column of this additional chain
-            node_new=group0._f_getChild(nodename)
-
-            # check columns have same dimensionality
-            if( len(node_0.shape) != len(node_new.shape) ):
-                raise ValueError ('node '+nodename+' dimension mis-match: chain0 = '+str(node_0.shape)+'; '+extra_chain_name+' = '+str(node_new.shape))
-
-            # append contents of this node on this chain to that on chain0
-            for index in np.arange(0,node_0.nrows):
-                node_0.append(node_new[index])
-
-        # remove chain from tracefile
-        chain._f_remove(recursive=True)
+#        # next deal with group0 table
+#        group0=chain.group0
+#
+#        # check nodes matches those of Chain0.group0
+#        if(group0._v_children.keys()!=hf.root.chain0.group0._v_children.keys()):
+#            raise ValueError ('children of '+extra_chain_name+' do not match chain0')
+#
+#        # loop through nodes of this chain's group0 group, appending to those of chain0
+#        for nodename in group0._v_children.keys():
+#
+#            # get this column of chain0
+#            node_0=hf.root.chain0.group0._f_getChild(nodename) 
+#
+#            # get this column of this additional chain
+#            node_new=group0._f_getChild(nodename)
+#
+#            # check columns have same dimensionality
+#            if( len(node_0.shape) != len(node_new.shape) ):
+#                raise ValueError ('node '+nodename+' dimension mis-match: chain0 = '+str(node_0.shape)+'; '+extra_chain_name+' = '+str(node_new.shape))
+#
+#            # append contents of this node on this chain to that on chain0
+#            for index in np.arange(0,node_0.nrows):
+#                node_0.append(node_new[index])
+#
+#        # remove chain from tracefile
+#        chain._f_remove(recursive=True)
 
 #############################################################################################
